367923e3e9936a3b20fbd3fdacd44e56b9b5099e
Alan Chen <alan.r.chen@gmail.com>
2017-11-02 16:29:14 -0700

    Gitting gud???

--- gridlogics/src/gridlogix.java
+++ gridlogics/src/gridlogix.java
@@ -1,2 +1,30 @@
+import java.util.Scanner;
+
 public class gridlogix {
+
+
+
+/*
+create 3d arrays of each row
+compare each element
+
+get maximum of each row  - push each one to new array
+
+take the new maximum array and get the minimum of the total arrays
+ */
+
+    Scanner ln = new Scanner(System.in);
+    int N = in.nextint();
+
+    for(int i = 0; i<N; i++){
+        if(b > a)
+        {
+        memory = b;
+        }
+        else
+        {
+        memory = a;
+        }
+
+
 }
