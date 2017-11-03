332a751c10e895eb6beaf26b8caf153ed39e0125
Alan Chen <alan.r.chen@gmail.com>
2017-11-03 09:56:45 -0700

    gitting guuder

--- CP2.1/src/calendarmagic.java
+++ CP2.1/src/calendarmagic.java
@@ -2,90 +2,28 @@ import java.util.Calendar;
 import java.util.GregorianCalendar;
 
 public class calendarmagic {
-    private String weekday;
+
+        }
 
     public static void main (String [] args) {
         GregorianCalendar cal = new GregorianCalendar();
-
         cal.add(Calendar.DAY_OF_MONTH, 100);
         int month = cal.get(Calendar.MONTH);
         int dayMonth = cal.get(Calendar.DAY_OF_MONTH);
-        int weekday = cal.get(Calendar.DAY_OF_WEEK);
+        int dayVal = cal.get(Calendar.DAY_OF_WEEK);
+        
         int year = cal.get(Calendar.YEAR);
-        String wkday = "";
+        String wkday = daymanager(dayVal);
 
 
-        public void daychanger(String weekday) {
-            this.weekday = weekday;
-            if (weekday == 1) {
-            wkday = "Sunday";
-        }
-
-        if (weekday == 2) wkday = "Monday";
-
-        if (weekday == 3) {
-            wkday = "Tuesday";
-        }
-
-        if (weekday == 4) {
-            wkday = "Wednesday";
-        }
-
-        if (weekday == 5) {
-            wkday = "Thursday";
-        }
-
-        if (weekday == 6) {
-            wkday = "Friday";
-        }
-
-        if (weekday == 7) {
-            wkday = "Saturday";
-        }
-        }
         System.out.println(String.format("%d / %d / %d | weekday: %s", month, dayMonth, year, wkday));
 
-
+        /*
         GregorianCalendar cal2 = new GregorianCalendar();
             cal2.set(2000,3,26);
-            int weekday2 = cal.get(Calendar.DAY_OF_WEEK);
-            String wkday2 = "";
-
-        if(weekday == 1)
-        {
-            wkday = "Sunday";
-        }
-
-        if(weekday == 2)
-        {
-            wkday = "Monday";
-        }
-
-        if(weekday == 3)
-        {
-            wkday = "Tuesday";
-        }
-
-        if(weekday == 4)
-        {
-            wkday = "Wednesday";
-        }
-
-        if(weekday == 5)
-        {
-            wkday = "Thursday";
-        }
-
-        if(weekday == 6)
-        {
-            wkday = "Friday";
-        }
-
-        if(weekday == 7)
-        {
-            wkday = "Saturday";
-        }
-
+            weekday = cal2.get(Calendar.DAY_OF_WEEK);
+            wkday = "";
+        */
 
 
     }
---
+++ CP2.1/src/dayMethod.java
@@ -0,0 +1,42 @@
+public class dayMethod {
+    public static String wkday;
+
+        String dayEdit(dayVal){
+
+        if (weekday == 1) {
+        wkday = "Sunday";
+        return wkday;
+        }
+
+        if (weekday == 2) {
+        wkday = "Monday";
+        return wkday;
+        }
+
+        if (weekday == 3) {
+        wkday = "Tuesday";
+        return wkday;
+        }
+
+        if (weekday == 4) {
+        wkday = "Wednesday";
+        return wkday;
+        }
+
+        if (weekday == 5) {
+        wkday = "Thursday";
+        return wkday;
+        }
+
+        if (weekday == 6) {
+        wkday = "Friday";
+        return wkday;
+        }
+
+        if (weekday == 7) {
+        wkday = "Saturday";
+        return wkday;
+        }
+        else
+        return "s";
+        }
